31e0aa58a20f7d5060587ca8b23eff767cc37540
Ramona Lazdina <ramona.lazdina@helmes.com>
2024-01-17 17:34:30 +0200

    fraction to fracty

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,7 +2,8 @@ import View from './View.js';
 
 //import icons from 'url:../../img/icons.svg'; //url: used for static assets
 import icons from '../../img/icons.svg'; //url: used for static assets
-import { Fraction } from 'fractional'; //any packages from npm -> no neet to specify path
+//import { Fraction } from 'fractional'; //any packages from npm -> no neet to specify path
+import fracty from 'fracty'; //any packages from npm -> no neet to specify path
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -134,7 +135,7 @@ class RecipeView extends View {
                 <use href="${icons}#icon-check"></use>
               </svg>
               <div class="recipe__quantity">${
-                ing.quantity ? new Fraction(ing.quantity).toString() : ''
+                ing.quantity ? fracty(ing.quantity).toString() : ''
               }</div>
               <div class="recipe__description">
                 <span class="recipe__unit">${ing.unit}</span>
